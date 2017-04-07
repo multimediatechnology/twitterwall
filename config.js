612a4da1397c87a72db60d310712adf27091cc41
Hannes Moser <hannes@impossiblearts.com>
2017-04-07 17:06:01 +0200

    refactor: cleanup of config file

--- config.js
+++ config.js
@@ -5,13 +5,13 @@
 var e = process.env
 
 module.exports = {
-  title: e.TITLE || '.concat() 2015 Twitterwall',
-  hashtag: e.HASHTAG || '#concat15',
-  port: e.PORT || 8000,
+  title: e.TITLE,
+  hashtag: e.HASHTAG,
+  port: e.PORT,
   twitter: {
     throttle: 1000,
-    tracks: (e.HASHTAGS || '#concat,#concat15,#concat2015').split(','),
-    users: (e.USERS || 'conc_at').split(','),
+    tracks: (e.HASHTAGS).split(','),
+    users: (e.USERS).split(','),
     tweetHistory: true,
     auth: {
       access_token: e.ACCESS_TOKEN,
@@ -40,62 +40,6 @@ module.exports = {
     blockPossiblySensitive: true
   },
   sponsors: [{
-    image: 'https://conc.at/images/logos/zalando@2x.png',
-    name: 'Zalando Technology',
-    duration: 20000
-  }, {
-    image: 'https://conc.at/images/logos/pusher@2x.png',
-    name: 'Pusher',
-    duration: 20000
-  }, {
-    image: 'https://conc.at/images/logos/braintree@2x.png',
-    name: 'Braintree Payments',
-    duration: 10000
-  }, {
-    image: 'https://conc.at/images/logos/aws@2x.png',
-    name: 'Amazon Web Services',
-    duration: 10000
-  }, {
-    image: 'https://conc.at/images/logos/travis@2x.png',
-    name: 'Travis CI',
-    duration: 10000
-  }, {
-    image: 'https://conc.at/images/logos/hoodie@2x.png',
-    name: 'Hoodie',
-    duration: 10000
-  }, {
-    image: 'https://conc.at/images/logos/mandrill@2x.png',
-    name: 'Mandrill',
-    duration: 10000
-  }, {
-    image: 'https://conc.at/images/logos/gg@2x.png',
-    name: 'gesagt.getan',
-    duration: 10000
-  }, {
-    image: 'https://conc.at/images/logos/siili@2x.png',
-    name: 'Siili',
-    duration: 10000
-  }, {
-    image: 'https://conc.at/images/logos/diamonddogs@2x.png',
-    name: 'the diamond:dogs|group',
-    duration: 10000
-  }, {
-    image: 'https://conc.at/images/logos/codeschool@2x.png',
-    name: 'codeschool',
-    duration: 10000
-  }, {
-    image: 'https://conc.at/images/logos/transloadit@2x.png',
-    name: 'Transloadit',
-    duration: 10000
-  }, {
-    image: 'https://conc.at/images/logos/techtrrrs@2x.png',
-    name: 'techtrrrs',
-    duration: 10000
-  }, {
-    image: 'https://conc.at/images/logos/cws@2x.jpg',
-    name: 'Coworking Salzburg',
-    duration: 10000
-  }, {
     image: 'https://conc.at/images/logos/fh@2x.jpg',
     name: 'University of Applied Sciences Salzburg',
     duration: 10000
